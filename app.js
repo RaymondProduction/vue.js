3d040bc23aa6707e401e1db286b8ebcfa9165fd8
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-10-03 13:36:47 +0300

    Added component

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-new Vue({
+var App = {
+  section_header : 'Main'
+}
+Vue.component('section-header', {
+  template: '<p>{{name}}</p>',
+  data: () => { return {name : App.section_header }},
+});
+Vue.component('menu-item', {
+  props: ['menu'],
+  template: '<li>{{ menu.name }}</li>'
+});
+var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!',
@@ -9,3 +20,5 @@ new Vue({
     ]
   }
 })
+
+
