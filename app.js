cf3c0c1406c7e5083d3f6fc75fde583b926f883f
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-10-03 14:51:35 +0300

    Connect routes

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // var App = {
 //   section_header : 'Main'
 // }
+
+const NotFound = { template: '<p>Not Found</p>' }
+const Home = { template: '<p>Main</p>' }
+const About = { template: '<p>About</p>' }
+const routes = [
+  { path: '/home', component: Home },
+  { path: '/about', component: About }
+];
+const router = new VueRouter({
+  routes
+})
+
 Vue.component('section-header', {
   props: ['name'],
   template: '<p>{{name}}</p>',
@@ -11,12 +23,17 @@ Vue.component('menu-item', {
 });
 var app = new Vue({
   el: '#app',
+  router : router,
   data: {
     message: 'Hello Vue.js!',
     menu: [
-      { name: 'Learn JavaScript' },
-      { name: 'Learn Vue' },
-      { name: 'Build something awesome' }
-    ]
+      {id: 1, name: 'Learn JavaScript' },
+      {id: 2, name: 'Learn Vue' },
+      {id: 3, name: 'Build something awesome' }
+    ],
   },
 })
+// const app = new Vue({
+//    el: '#app',
+//   router : router
+// })
