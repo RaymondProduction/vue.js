e3c422f35808c10181a059bab937c77730096aaa
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-10-03 13:17:49 +0300

    Added list

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    menu: [
+      { name: 'Learn JavaScript' },
+      { name: 'Learn Vue' },
+      { name: 'Build something awesome' }
+    ]
   }
 })
