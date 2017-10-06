ae80d6ff926e562e57788984e6e7b1890c10f3bf
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-10-06 07:44:21 +0300

    Added effect

--- js/app.js
+++ js/app.js
@@ -7,10 +7,10 @@ const ppPage = { template: '#pp' }
 const spPage = { template: '#sp' }
 const mainPage = { template: '<h1>Hello</h1>'}
 const routes = [
-  { path: '/oop', component: oopPage },
-  { path: '/pp', component: ppPage },
-  { path: '/sp', component: spPage },
-  { path: '/', component: mainPage}
+  { path: '/oop', component: oopPage, props: { position: 1} },
+  { path: '/pp', component: ppPage, props: { position: 2} },
+  { path: '/sp', component: spPage, props:  () => { /*this.menu.position=3 */} },
+  { path: '/', component: mainPage, props: () => { console.log('YES');}}
 ];
 const router = new VueRouter({
   routes
@@ -20,19 +20,38 @@ Vue.component('section-header', {
   props: ['name'],
   template: '<p>{{name}}</p>',
 });
-Vue.component('menu-item', {
-  props: ['menu'],
-  template: '<a v-bind:href="menu.path"> {{menu.name}} </a></li>'
+Vue.component('menu-main',{
+  props: ['menu','position'],
+  template: '<table class="table-menu"><tr>'+
+            '  <td class="td" v-for="item in menu.list"  v-if="menu.position != item.path" v-bind:width = "item.width"><a  class="a" v-bind:href="item.path"> {{item.name}} </a></td>'+
+            '  <td class="td" style="background: #97D435;" v-for="item in menu.list"  v-if="menu.position == item.path" v-bind:width = "item.width"><a  class="a" v-bind:href="item.path"> {{item.name}} </a></td>'+
+            '</tr></table>',
 });
 var app = new Vue({
   el: '#app',
   router : router,
   data: {
     message: 'Hello Vue.js!',
-    menu: [
-      {id: 1, path : "#oop", name: 'Object-oriented programming' },
-      {id: 2, path : "#sp", name: 'Structured programming' },
-      {id: 3, path : "#pp",name: 'Procedural programming' }
-    ],
+    menu: {
+      position : 1,
+      list: [
+          {id: 1, path : "#oop", width: 120, name: 'Про мене' },
+          {id: 2, path : "#sp",  width: 130, name: 'Файли' },
+          {id: 3, path : "#pp",  width: 90,  name: 'Відео' },
+          {id: 4, path : "#pp",  width: 140, name: 'Фотогалерея' },
+          {id: 5, path : "#pp",  width: 200, name: 'Зворотній зв\'язок' },
+          {id: 6, path : "#pp",  width: 120, name: 'Різне' },
+      ],
+    },
+  },
+  methods: {
+    menuPos: function () {
+      this.menu.position=0;
+    }
+  },
+    watch: {
+    '$route': function (id) {
+      this.menu.position = id.path.replace('/','#');
+    }
   },
 })
