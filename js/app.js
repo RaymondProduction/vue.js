d7662c0646364108c08ca301f79a3dcdd2cd8c60
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-02-11 01:15:35 +0200

    Clear logs

--- js/app.js
+++ js/app.js
@@ -23,7 +23,6 @@ var app = new Vue({
   },
   methods: {
     filter(camera, street){
-      console.log(this.validation);
       if (this.validation){
         return camera.a!== '';
       } else {
@@ -63,8 +62,6 @@ var app = new Vue({
           name: periods[number]
         })
       }
-      console.log(result );
-      
       return result;
     }
   },
