ef6fb116258fabbde96ed3d23f205ec9f156000f
raymond <Raymond Perminov>
2018-02-20 13:33:47 +0200

    Fix time

--- js/app.js
+++ js/app.js
@@ -15,11 +15,13 @@ Vue.component('camera', {
       default: function(){
         var i = _.findIndex(this.selected, (cameraSelected)=>cameraSelected.uid == this.camera.uid.toString() );
         if (i>-1) {
-          var d = new Date(this.selected[i].end_services);
+          console.log('Time:',this.selected[i].end_services)
+          var d = new Date(this.selected[i].end_services*1000);
           var date = d.getDate();
           var month = d.getMonth() + 1;
           var year = d.getFullYear();
-          var t = date + "."+ month + "." + year + "." ;
+          var t = date + "."+ month + "." + year;
+          console.log(t);
           return {
             description: this.selected[i].description,
             end_services: t,
